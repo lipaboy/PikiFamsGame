15addedab48546497a001a36fe8d3da093731e4d
Artem Lipatkin <lipatkin96@gmail.com>
2017-08-18 00:25:52 +0700

    Add typedef Numbers (uint32_t)

--- gamebot.h
+++ gamebot.h
@@ -2,14 +2,26 @@
 #define GAMEBOT_H
 
 #include <iostream>
+#include <set>
 
 #include "GameCreator.h"
 
 namespace PikiFamsGameBot {
 
-	using namespace PikiFamsGame;
+	using PikiFamsGame::GameCreator;
+	using PikiFamsGame::Numbers;
+	using PikiFamsGame::Array4Digits;
+	using PikiFamsGame::GameStepInfo;
+	using std::set;
 
+	//Special sets
 
+	class GameSet {
+	public:
+
+	private:
+		//set<Numbers> 
+	};
 
 	//return game step count
 	uint32_t solveTheGame(GameCreator game);
