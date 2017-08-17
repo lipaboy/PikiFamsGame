55d2cf144e9354544008f9cc38f8680b0e8a4f67
Artem Lipatkin <lipatkin96@gmail.com>
2017-08-17 23:49:53 +0700

    Remove getting residue by 10. Write method guess

--- GameCreator.cpp
+++ GameCreator.cpp
@@ -3,10 +3,25 @@
 #include <algorithm>
 
 namespace PikiFamsGame {
-	GameCreator::GameCreator(const Array4Digits& _guessingNumber)
+	GameCreator::GameCreator(const Array4Digits& _guessingNumber) : guessingNumber(_guessingNumber)
 	{
-		std::transform(_guessingNumber.cbegin(), _guessingNumber.cend(),
-			guessingNumber.begin(), [](int digit) -> int { return digit % 10; });
+		//TODO: all the digits must be different
+	/*	std::transform(_guessingNumber.cbegin(), _guessingNumber.cend(),
+			guessingNumber.begin(), [](int digit) -> int { return digit % 10; });*/
+	}
+	GameStepInfo GameCreator::guess(const Array4Digits & likelyNumber)
+	{
+		GameStepInfo info = { 0, 0 };
+
+		stepCount++;
+
+		for (uint32_t i = 0; i < guessingNumber.size(); i++) {
+			for (uint32_t j = 0; j < likelyNumber.size(); j++) {
+				if (guessingNumber[i] == likelyNumber[i])
+			}
+		}
+
+		return info;
 	}
 }
 
--- GameCreator.h
+++ GameCreator.h
@@ -11,16 +11,25 @@ namespace PikiFamsGame {
 	//You can use digits from 0 to 9
 	//TODO: may be create set<int> with this numbers
 	typedef std::array<int, 4> Array4Digits;
-	std::set<int> Digits({ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 });
+	//std::set<int> Digits({ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 });
+
+	struct GameStepInfo;
 
 	class GameCreator {
 	public:
 		//4-digit number
 		GameCreator(const Array4Digits& _guessingNumber);
 
+		GameStepInfo guess(const Array4Digits& likelyNumber) const;
 
 	private:
 		Array4Digits guessingNumber;
+		uint32_t stepCount = 0;
+	};
+
+	struct GameStepInfo {
+		uint32_t piki;
+		uint32_t fams;
 	};
 
 }
