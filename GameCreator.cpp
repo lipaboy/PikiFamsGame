15fe23ad8b1ef0d3c12adb0592fb954dddc304ca
Artem Lipatkin <lipatkin96@gmail.com>
2017-08-17 23:41:14 +0700

    Rewrite constructor. Add getting residue by 10 for accuracy

--- GameCreator.cpp
+++ GameCreator.cpp
@@ -1,7 +1,12 @@
 #include "GameCreator.h"
 
+#include <algorithm>
+
 namespace PikiFamsGame {
-
-
+	GameCreator::GameCreator(const Array4Digits& _guessingNumber)
+	{
+		std::transform(_guessingNumber.cbegin(), _guessingNumber.cend(),
+			guessingNumber.begin(), [](int digit) -> int { return digit % 10; });
+	}
 }
 
--- GameCreator.h
+++ GameCreator.h
@@ -2,16 +2,22 @@
 #define GAME_CREATOR_H
 
 #include <array>
+#include <set>
 
 namespace PikiFamsGame { 
 
 	using std::array;
+
+	//You can use digits from 0 to 9
+	//TODO: may be create set<int> with this numbers
 	typedef std::array<int, 4> Array4Digits;
+	std::set<int> Digits({ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 });
 
 	class GameCreator {
 	public:
 		//4-digit number
-		GameCreator(Array4Digits _guessingNumber) : guessingNumber(_guessingNumber) {}
+		GameCreator(const Array4Digits& _guessingNumber);
+
 
 	private:
 		Array4Digits guessingNumber;
