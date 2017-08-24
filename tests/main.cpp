ae901bc7ae6fdd491131e81389f4fbece84e6a14
Artem Lipatkin <lipatkin96@gmail.com>
2017-08-24 22:41:40 +0700

    Rename main_tests.cpp to main.cpp. Now I work with Test Project in one place

---
+++ tests/main.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+
+#define TEST_MODE
+
+#ifdef TEST_MODE
+#include "Test_hd.h"
+
+#else
+#include "GameCreator.h"
+#include "gamebot.h"
+
+using namespace PikiFamsGame;
+using namespace PikiFamsGameBot;
+#endif
+
+
+//using namespace std;
+
+int main(int argc, char *argv[])
+{
+#ifdef TEST_MODE
+	::testing::InitGoogleTest(&argc, argv);
+	RUN_ALL_TESTS();
+#else
+
+	using std::cout;
+	using std::endl;
+
+	GameCreator game({ 1, 2, 3, 4 });
+	GameCreator game2;
+
+	//--------------------Game Bot Try----------------------------//
+
+	//solveTheGame(game);
+
+	//cout << info.piki << "piki " << info.fams << "fams" << endl;
+
+	//--------------------User mode----------------------------//
+
+	DigitArray userStep;
+	for (; ; ) {
+		cout << "Enter the number: ";
+		DigitArray::iterator it;
+		for (it = userStep.begin(); it != userStep.end(); it++) {
+			std::cin >> *it;
+		}
+		GameStepInfo info = game2.guess(userStep);
+		cout << info.piki << "p " << info.fams << "f " << endl;
+		if (info.piki >= 4 || userStep[0] == userStep[1])
+			break;
+	}
+
+#endif
+
+	return 0;
+}
