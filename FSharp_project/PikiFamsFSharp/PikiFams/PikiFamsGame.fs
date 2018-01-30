43b81ed8cb7647a3e3201174e35ff796ca5cbd72
Artem Lipatkin <lipatkin96@gmail.com>
2018-01-30 12:15:10 +0700

    Add FSharp project

---
+++ FSharp_project/PikiFamsFSharp/PikiFams/PikiFamsGame.fs
@@ -0,0 +1,29 @@
+﻿namespace PikiFams
+
+open System
+
+module PikiFamsGame =
+    let digits = [0 .. 9]
+
+    let CORTEGE_LEN = 4;
+    type GameCortege = Array
+    
+    let getRandItem list =
+        let rand = new Random()
+        let len = List.length list
+        list |> List.item (rand.Next() % len)
+    
+    let randCortege list = 
+        let rec loop list =
+            seq { 
+                let item = getRandItem list
+                yield item
+                yield! (List.except [item] list |> loop)
+                }
+        loop list
+    
+    let getRandCortege =
+        Seq.toArray (Seq.take CORTEGE_LEN (randCortege digits))
+
+    //type GameCreator
+
---
+++ FSharp_project/PikiFamsFSharp/Program.fs
@@ -0,0 +1,15 @@
+﻿namespace PikiFamsEntryPoint
+
+open PikiFams.PikiFamsGame
+
+module Main =
+
+    type MyClass(stop: int, start: int) =
+        member me.Stop = stop
+        member me.Start = start
+
+    [<EntryPoint>]
+    let main argv = 
+        getRandCortege |> Array.iter (printf "%d ")
+
+        0 // return an integer exit code
