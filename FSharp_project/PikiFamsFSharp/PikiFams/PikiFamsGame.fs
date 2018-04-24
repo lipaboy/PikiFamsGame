a5e6ccc126a314ec75dec535e5b2bfd7b24ce0d8
Artem Lipatkin <lipatkin96@gmail.com>
2018-04-24 16:52:25 +0700

    Write GameCreator on F#

--- FSharp_project/PikiFamsFSharp/PikiFams/PikiFamsGame.fs
+++ FSharp_project/PikiFamsFSharp/PikiFams/PikiFamsGame.fs
@@ -2,14 +2,28 @@
 
 open System
 
+// PLAN
+//------
+// TODO: connect the tests
+//-----
+// READ
+//-----
+// READ: about inheritance
+// READ: about namespaces and scopes
+// READ: about inner fields
+
 module PikiFamsGame =
+    open System.Security.Cryptography.X509Certificates
+    open System.Globalization
+
     let digits = [0 .. 9]
 
     let CORTEGE_LEN = 4;
     type GameCortege = Array
     
+    let rand = new Random()
+
     let getRandItem list =
-        let rand = new Random()
         let len = List.length list
         list |> List.item (rand.Next() % len)
     
@@ -22,8 +36,55 @@ module PikiFamsGame =
                 }
         loop list
     
-    let getRandCortege =
+    let getRandCortege() =
         Seq.toArray (Seq.take CORTEGE_LEN (randCortege digits))
+
+    //---------------GameCreator-------------//
+
+    type GameTurnInfo(pikis : int, famas : int) =
+        member this.Pikis = pikis
+        member this.Famas = famas
+
+    type GameCreator(guessingNumber:GameCortege) =
+        let guessingNumber = guessingNumber
+        let mutable turnCount_ = 0
+        
+        let makeTurn() = turnCount_ <- turnCount_ + 1
+        member this.TurnCount() = turnCount_
+        //TODO: throw exception if length is not appropriate
+        
+        new() =
+            let cort = getRandCortege()
+            cort |> Array.iter (printf "%d ")
+            printfn ""
+            GameCreator(cort)
+
+        member this.Guess(likelyNumber:GameCortege) =
+            makeTurn()
+            let mutable pikis = 0
+            let mutable famas = 0
+            for i in 0 .. guessingNumber.Length - 1 do
+                for j in 0 .. likelyNumber.Length - 1 do
+                    if (likelyNumber.GetValue(j) = guessingNumber.GetValue(i)) then
+                        if (i = j) then
+                            pikis <- pikis + 1
+                        else
+                            famas <- famas + 1
+            new GameTurnInfo(pikis, famas)
+
+    //----------Tests-------------//
+
+    let testGuess() =
+        let game = new GameCreator([|1; 2; 3; 4|])
+        let info = game.Guess([|2; 1; 3; 0|])
+        assert(info.Famas = 2)
+        assert(info.Pikis = 1)
+
+    let runAllTheTests =
+        testGuess()
+
+            
+
 
     //type GameCreator
 
--- FSharp_project/PikiFamsFSharp/Program.fs
+++ FSharp_project/PikiFamsFSharp/Program.fs
@@ -5,11 +5,34 @@ open PikiFams.PikiFamsGame
 module Main =
 
     type MyClass(stop: int, start: int) =
+        let mutable prop = 1
+        let mutable _key = 5
+
+        //extra code to be done at construction time
+        do printfn "the stop equals %i" stop
+
+        member mem.setKey (x:int) = 
+            _key <- x    
         member me.Stop = stop
         member me.Start = start
 
+        member this.MyProp 
+            with get() = prop 
+            and set(value) =  prop <- value
+        member val ReadWriteAuto = 0 with get, set
+
     [<EntryPoint>]
     let main argv = 
-        getRandCortege |> Array.iter (printf "%d ")
+
+        let myClass = new MyClass(3, 4)
+        myClass.setKey 228
+        myClass.MyProp <- 5
+        printfn "%i" myClass.MyProp
+        myClass.ReadWriteAuto <- 7
+        printfn "%i" myClass.ReadWriteAuto
+
+        let game = new GameCreator()
+        let info = game.Guess [|1 .. 4|]
+        printfn "Info: %d pikis, %d famas" info.Pikis info.Famas
 
         0 // return an integer exit code
